44d5eb173a0856558a6249d902384f557c1b9a91
Dave Kalu <davekalu46@gmail.com>
2019-12-16 10:42:57 +0100

    Updates

---
+++ precache-manifest.e715de825361570146b05b823ecc00ab.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18f62f3848dec6ad944090a50a58078b",
+    "url": "/adludio/index.html"
+  },
+  {
+    "revision": "9646f3343fd51fd1b44e",
+    "url": "/adludio/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "66d291695db3e342a6f6",
+    "url": "/adludio/static/css/main.0e9c6bfc.chunk.css"
+  },
+  {
+    "revision": "9646f3343fd51fd1b44e",
+    "url": "/adludio/static/js/2.c13404fb.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/adludio/static/js/2.c13404fb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "66d291695db3e342a6f6",
+    "url": "/adludio/static/js/main.f1a627c7.chunk.js"
+  },
+  {
+    "revision": "cf53bb9e1968eb3ba2cd",
+    "url": "/adludio/static/js/runtime-main.ed7ede22.js"
+  },
+  {
+    "revision": "aa6ffae2de9588343be7d5b990495ac3",
+    "url": "/adludio/static/media/asset1.aa6ffae2.jpg"
+  },
+  {
+    "revision": "585afc787e1475b6bb6ff30887580c53",
+    "url": "/adludio/static/media/asset2.585afc78.png"
+  }
+]);
